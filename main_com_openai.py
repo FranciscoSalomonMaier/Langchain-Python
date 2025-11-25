ed7718d9256c7f65d75f674e04f51391d82b479f
FranciscoSMWork <franciscosmwork@outlook.com>
2025-11-25 16:41:38 -0300

    ### Updated - Complex chain

--- main_com_openai.py
+++ main_com_openai.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from langchain_core.globals import set_debug
 import os
 
+set_debug(True)
+
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -14,8 +16,12 @@ class Destino(BaseModel):
     cidade:str = Field("A cidade recomendada para visitar")
     motivo:str = Field("motivo pelo qual é interessante visitar essa cidade")
 
+class Restaurantes(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    restaurantes:str = Field("Restaurantes recomndados na cidade")
 
-parseador = JsonOutputParser(pydantic_object=Destino) ### pydantic difene o formato da resposta de saída, que é em formato json
+parseador_destino = JsonOutputParser(pydantic_object=Destino) ### pydantic difene o formato da resposta de saída, que é em formato json
+parseador_restaurante = JsonOutputParser(pydantic_object=Restaurantes)
 
 prompt_cidade = PromptTemplate(
     template =""" 
@@ -23,8 +29,21 @@ prompt_cidade = PromptTemplate(
     {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables = {"formato_de_saida": parseador.get_format_instructions()}
+    partial_variables = {"formato_de_saida": parseador_destino.get_format_instructions()}
 )
+
+prompt_restaurantes = PromptTemplate(
+    template =""" 
+    Sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables = {"formato_de_saida": parseador_restaurante.get_format_instructions()}
+)
+
+prompt_cultural = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}"
+)
+
 
 modelo = ChatOpenAI(
     model="gpt-3.5-turbo",
@@ -32,7 +51,12 @@ modelo = ChatOpenAI(
     api_key=api_key,
 )
 
-cadeia = prompt_cidade | modelo | parseador
+
+cadeia_1 = prompt_cidade | modelo | parseador_destino
+cadeia_2 = prompt_restaurantes | modelo | parseador_restaurante
+cadeia_3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = (cadeia_1 | cadeia_2 | cadeia_3)
 
 resposta = cadeia.invoke({
     "interesse": "praias"
