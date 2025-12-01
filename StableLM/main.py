1d928fff269a6dc85fd92fdc17632bcd917003ab
FranciscoSMWork <franciscosmwork@outlook.com>
2025-12-01 14:09:38 -0300

    ### Added - Stable model

---
+++ StableLM/main.py
@@ -0,0 +1,28 @@
+#stabilityai/stablelm-2-1_6b
+
+from transformers import pipeline
+from langchain_huggingface import HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.globals import set_debug
+
+set_debug(True)
+
+model = pipeline(
+    task="text-generation",
+    model="stabilityai/stablelm-2-1_6b",
+    max_length=256,
+    truncation=True
+)
+
+llm = HuggingFacePipeline(pipeline=model)
+
+# Create the prompt template
+template = PromptTemplate.from_template("Explain {topic} in detail for a {age} year old to understand")
+# template = PromptTemplate.from_template("Explique em português: {topic} em detalhes, de forma que uma criança de {age} anos consiga entender.")
+
+chain = template | llm
+topic = input("Topic: ")
+age = input("Age: ")
+
+response = chain.invoke({"topic": topic, "age":age})
+print(response)
