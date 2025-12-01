445f5d7fd6a9a88e76bf833b42843a55cd8f19df
FranciscoSMWork <franciscosmwork@outlook.com>
2025-12-01 15:46:09 -0300

    ### Changed - Langgraph version 3

--- OpenAI/main_langgraph.py
+++ OpenAI/main_langgraph.py
@@ -3,6 +3,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from typing import Literal, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+import asyncio
 import os
 
 load_dotenv()
@@ -40,10 +43,40 @@ prompt_roteador = ChatPromptTemplate.from_messages(
 
 roteador = prompt_roteador | modelo.with_structured_output(Rota)
 
-def responder(pergunta: str):
-    rota = roteador.invoke({"query":pergunta})["destino"]
-    if rota == "praia":
-        return cadeia_praia.invoke({"query": pergunta})
-    return cadeia_montanha.invoke({"query": pergunta})
+class Estado(TypedDict):
+    query:str
+    destino:Rota
+    resposta:str
 
-print(responder("Quero passear por praias belas pelo Brasil"))
+async def no_roteador(estado: Estado, config=RunnableConfig):
+    return {"destino": await roteador.ainvoke({"query": estado["query"]}, config)}
+
+async def no_praia(estado: Estado, config=RunnableConfig):
+    return {"destino": await cadeia_praia.ainvoke({"query": estado["query"]}, config)}
+
+async def no_montanha(estado: Estado, config=RunnableConfig):
+    return {"destino": await cadeia_montanha.ainvoke({"query": estado["query"]}, config)}
+
+def escolher_no(estado:Estado)->Literal["praia", "montanha"]:
+    return "praia" if estado["destino"]["destino"] == "praia" else "montanha"
+
+#Cria Grafo
+grafo = StateGraph(Estado)
+grafo.add_node("rotear", no_roteador)
+grafo.add_node("praia", no_praia)
+grafo.add_node("montanha", no_montanha)
+
+grafo.add_edge(START, "rotear")
+grafo.add_conditional_edges("rotear", escolher_no)
+grafo.add_edge("praia", END)
+grafo.add_edge("montanha", END)
+
+app = grafo.compile()
+
+async def main():
+    resposta = await app.ainvoke(
+        {"query": "Quero vistar um lugar no Brasil famoso por praias e cultura"}
+    )
+    print(resposta["resposta"])
+
+asyncio.run(main())
